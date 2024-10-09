957df5b172d771cfcda9ad0418cf7f200de9db38
Baloo <73783549+41Baloo@users.noreply.github.com>
2024-10-09 06:02:42 +0000

    Performance improvements, Fixed potential race conditions

--- intigration/balooPow.go
+++ intigration/balooPow.go
@@ -4,57 +4,100 @@ import (
 	"encoding/hex"
 	"math/rand"
 	"strconv"
+	"sync"
+	"time"
 
 	sha256 "github.com/minio/sha256-simd"
 )
 
-// Creates a new pow pool that will hold pows that all have the given salt length. A minimum length of 10 is recommended
-func NewPowPool(saltLength int) *BalooPowPool {
+// Creates a new pow pool that will hold pows that all have the given salt length. A minimum length of 10 is recommended. Pows that have not been manually deleted will be deleted after secondsValid seconds.
+func NewPowPool(saltLength int, secondsValid int64) *BalooPowPool {
 	pool := BalooPowPool{
-		pows:       map[string]BalooPow{},
+		pows:       map[string]*BalooPow{},
 		saltLength: saltLength,
+
+		lastUnix:    time.Now().Unix(),
+		powDuration: secondsValid,
+
+		mutex: &sync.RWMutex{},
 	}
 
+	go pool.deleteOutdatedPows()
+
 	return &pool
+}
+
+func (pool *BalooPowPool) deleteOutdatedPows() {
+	for {
+		pool.lastUnix = time.Now().Unix()
+
+		pool.mutex.Lock()
+		for identifier, pow := range pool.pows {
+			if pow.Created+pool.powDuration < pool.lastUnix {
+				delete(pool.pows, identifier)
+			}
+		}
+		pool.mutex.Unlock()
+
+		time.Sleep(5 * time.Second)
+	}
 }
 
 /*
 Will generate a new pow for the given identifier, add it to the pool and return the generated pow
 WARNING: This will overwrite previously generated pow's with the same identifier
 */
-func (pool *BalooPowPool) NewPow(identifier string, difficulty int) BalooPow {
+func (pool *BalooPowPool) NewPow(identifier string, difficulty int) *BalooPow {
 	salt := randStr(pool.saltLength)
 	solution := rand.Intn(difficulty)
 	challenge := sha256.Sum256([]byte(salt + strconv.Itoa(solution)))
 
-	pow := BalooPow{
+	pow := &BalooPow{
 		Salt:       salt,
 		Solution:   solution,
 		Challenge:  hex.EncodeToString(challenge[:]),
 		Difficulty: difficulty,
+
+		Created: pool.lastUnix,
 	}
 
+	pool.mutex.Lock()
 	pool.pows[identifier] = pow
+	pool.mutex.Unlock()
+
 	return pow
 }
 
 // Will delete a pow from the pool via its identifier
 func (pool *BalooPowPool) DeletePow(identifier string) {
+	pool.mutex.Lock()
 	delete(pool.pows, identifier)
+	pool.mutex.Unlock()
 }
 
 // Check if a given pow exists within the pool
 func (pool *BalooPowPool) DoesPowExist(identifier string) bool {
+	pool.mutex.RLock()
 	_, found := pool.pows[identifier]
+	pool.mutex.RUnlock()
+
 	return found
 }
 
 // Check if the provided solution is valid for the given identifier (will also return false if the identifier doesn't exist)
 func (pool *BalooPowPool) IsValidSolution(identifier string, solution int) bool {
+	pool.mutex.RLock()
 	pow, found := pool.pows[identifier]
+	pool.mutex.RUnlock()
+
 	if !found {
 		return false
 	}
+
+	if pow.Created+pool.powDuration < pool.lastUnix { // Pow expired. Nothing else to do. Our routine will clean it up
+		return false
+	}
+
 	return pow.IsValidSolution(solution)
 }
 
--- intigration/structs.go
+++ intigration/structs.go
@@ -1,13 +1,21 @@
 package balooPow
 
+import "sync"
+
 type BalooPow struct {
 	Salt       string
 	Solution   int
 	Challenge  string
 	Difficulty int
+
+	Created int64
 }
 
 type BalooPowPool struct {
 	saltLength int
-	pows       map[string]BalooPow
+	pows       map[string]*BalooPow
+
+	lastUnix    int64
+	powDuration int64
+	mutex       *sync.RWMutex
 }
